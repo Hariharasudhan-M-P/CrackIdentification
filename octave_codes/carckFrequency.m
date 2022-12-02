4200d02235c2bab6012723add8584af756a6442f
Aman Gautam <13annon@gmail.com>
2022-12-02 13:36:23 +0530

    excel

--- octave_codes/carckFrequency.m
+++ octave_codes/carckFrequency.m
@@ -2,16 +2,16 @@ close all
 clear
 clc
 
-crackRatio = [0,.1,.2,.3,.4,.5,.6,.7,.8,.9];
+crackRatio = [0,.2,.4,.6,.8];
 naturalFrequencys = [];
-for crackR = 1:10
-  crackLength = crackRatio(crackR)*.5;
+for kkk = 1:5
+  crackLength = crackRatio(kkk)*.5;
   area  = .5*.5;
-  totalLength = 3;
+  totalLength = 30;
   numberOfNodes = 30;
   rho = 7850;
   numberOfElements = numberOfNodes-1;
-  moiCrack = (.5*(.5-crackLength)^3)/12;
+  moiCrack = ((.5-crackLength)^4)/12;
   elasticityModulus = 2*10^11;
   I = (.5^4)/12;
   KG = zeros(2*numberOfNodes,2*numberOfNodes);
@@ -54,7 +54,7 @@ for crackR = 1:10
   KG(1,1) = 1;
   KG(ibcV2,ibcV2) = 1;
   cracked = sqrt(eig(KG,M));
-  naturalFrequencys(crackR) = cracked(3);
+  naturalFrequencys(kkk) = cracked(4);
 end
 crackRatio'
 naturalFrequencys'
